d88b8df53ffc39d7ff55bf98f55d0d0ce20b2828
champix56 <desorbaix@free.fr>
2025-11-25 12:11:35 +0100

    forme des props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,21 @@
-import './App.css'
-import Button from './components/ui/Button/Button';
+import "./App.css";
+import Button from "./components/ui/Button/Button";
 function App() {
-  return <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-  </div>
+  return (
+    <div>
+      <Button
+        titre="Button1"
+        boolean={true}
+        number={245}
+        str={`khgds${new Date().toISOString()}dcqssd`}
+        function={() => {
+          console.log("coucou");
+        }}
+        object={{ abc: 123, bcf: "toto" }}
+        array={[1, 2, 3, 4]}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
