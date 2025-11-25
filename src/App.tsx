3923f195f6183278e7bff35eb597ae1c8b951f17
champix56 <desorbaix@free.fr>
2025-11-25 11:57:23 +0100

    Premier composant

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './App.css'
-
-function App() {
-  return <div>DEMAT BREIZH</div>
-}
-
-export default App
---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from './components/ui/Button/Button';
+function App() {
+  return <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+  </div>
+}
+
+export default App
