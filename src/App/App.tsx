5cabd78f3cade8937508e631e38f154209c6c2e0
champix56 <desorbaix@free.fr>
2025-11-25 16:46:27 +0100

    cycle de vie

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/ui/Button/Button";
 function App() {
+  const [counter, setCounter] = useState(-1);
+
+  useEffect(() => {
+    console.log('dans le useEffect',counter)
+    return () => {
+      //fonction de demontage
+    };
+  }, [counter])
+  useEffect(() => {
+    //montage initial 
+    setCounter(0);
+  }, [])
   return (
     <div>
-      <Button type="button" color="skyblue" onButtonClick={()=>{
-        console.log('-1 clické');
-      }}>-1</Button>
-      <Button color="aquamarine"  onButtonClick={()=>{
-        console.log('+1 clické');
-      }}>+1</Button>
+      <div style={{ textAlign: "center" }}>counter: {counter}</div>
+      <Button
+        type="button"
+        color="skyblue"
+        onButtonClick={() => {
+          console.log("-1 clické");
+          setCounter(counter - 1);
+          console.log(counter);
+        }}
+      >
+        -1
+      </Button>
+      <Button
+        color="aquamarine"
+        onButtonClick={() => {
+          console.log("+1 clické");
+          setCounter(counter+1);
+          console.log(counter);
+        }}
+      >
+        +1
+      </Button>
     </div>
   );
 }
