434dc84ca0b74ff9f166d6e6565c5f5ab98b5f1a
champix56 <desorbaix@free.fr>
2025-11-26 23:47:42 +0100

    gestion param avec store

--- src/App/pages/Editor.tsx
+++ src/App/pages/Editor.tsx
@@ -1,9 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
 import FlexH1stGrow from "../components/layout/FlexH1stGrow/FlexH1stGrow";
 import MemeForm from "../components/MemeForm/MemeForm.store";
 import MemeSvgViewer from "../components/ui/MemeSVGViewer/MemeSVGViewer.store";
-
+import type { AppDispatch, RootState } from "../store/store";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import { clear, update } from "../store/current";
 
 const Editor = () => {
+  const d: AppDispatch = useDispatch();
+  const params=useParams();
+  const memes=useSelector((s:RootState)=>s.ressources.memes)
+  const navigation=useNavigate()
+  useEffect(() => {
+    if(undefined!==params.id){
+      const memeFound=memes.find(m=>m.id===Number(params.id))
+      if(undefined!==memeFound){
+        d(update(memeFound))}
+      else{
+         navigation('/edit')
+      }
+    }
+    else{
+      d(clear())
+    }
+  }, [d, params,memes, navigation]);
   return (
     <FlexH1stGrow>
       <MemeSvgViewer basePath="" />
