7c2c7aee9c8381040ff81b11860118adfd7d838f
champix56 <desorbaix@free.fr>
2025-11-26 21:57:28 +0100

    gestion des liens sous react-bootstrap

--- src/App/components/ui/Navbar/Navbar.tsx
+++ src/App/components/ui/Navbar/Navbar.tsx
@@ -3,16 +3,17 @@ import styles from './Navbar.module.css'
 import { Container, Nav, Navbar as Nvb } from 'react-bootstrap'
 
 import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+import { Link } from 'react-router'
 const Navbar :React.FC= () => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
        <Nvb bg="dark" data-bs-theme="dark">
         <Container>
-          <Nvb.Brand href="#home">Navbar</Nvb.Brand>
+          <Link className='navbar-brand' to="/">Navbar</Link>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link className='nav-link' to="/">Home</Link>
+            <Link className='nav-link' to="/thumbnail">thumbnail</Link>
+            <Link className='nav-link' to="/edit">Nouveau meme ...</Link>
           </Nav>
         </Container>
       </Nvb>
