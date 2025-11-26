bfb47ea5eeb03632786885ffffd05d2c95256e74
champix56 <desorbaix@free.fr>
2025-11-26 21:52:39 +0100

    liens simple

--- src/App/pages/Home.tsx
+++ src/App/pages/Home.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div>Bonjour et bienvenue voic les action possible sur ce site :</div>
+      <ul>
+        <li>
+          <Link to={"/edit"}>Nouveau</Link>
+        </li>
+         <li>
+          <Link to={"/thumbnail"}>thumbnail</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
