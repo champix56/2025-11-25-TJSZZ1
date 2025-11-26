44e31b6d06ca52318270ea028d1e5b881a8c1550
champix56 <desorbaix@free.fr>
2025-11-26 21:50:29 +0100

    gestion des routes simples

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,20 +2,22 @@ import "./App.css";
 import FlexV3rdGrow from "./components/layout/FlexV3rdGrow/FlexV3rdGrow";
 import Navbar from "./components/ui/Navbar/Navbar";
 import Header from "./components/ui/Header/Header";
-import FlexH1stGrow from "./components/layout/FlexH1stGrow/FlexH1stGrow";
-import MemeForm from "./components/MemeForm/MemeForm.store";
 import Footer from "./components/ui/Footer/Footer";
-import MemeSvgViewer from "./components/ui/MemeSVGViewer/MemeSVGViewer.store";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Thumbnail from "./pages/Thumbnail";
+import Editor from "./pages/Editor";
 
 function App() {
   return <div className="App">
     <FlexV3rdGrow>
       <Header/>
       <Navbar/>
-      <FlexH1stGrow>
-        <MemeSvgViewer basePath=""/>
-        <MemeForm />
-      </FlexH1stGrow>
+        <Routes>
+          <Route Component={Home} path="/" />
+          <Route Component={Thumbnail} path="/thumbnail" />
+          <Route Component={Editor} path="/edit" />
+        </Routes>
       <Footer/>
     </FlexV3rdGrow>
   </div>
