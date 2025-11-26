c4c3cbdd0990f815661d9b65b4e154564ad4fb25
champix56 <desorbaix@free.fr>
2025-11-26 18:26:56 +0100

    chargement en state des images

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,18 +5,25 @@ import Header from "./components/ui/Header/Header";
 import FlexH1stGrow from "./components/layout/FlexH1stGrow/FlexH1stGrow";
 import MemeForm from "./components/MemeForm/MemeForm";
 import Footer from "./components/ui/Footer/Footer";
-import { emptyMeme, MemeSVGViewer, type MemeInterface } from "orsys-tjs-meme";
-import { useState } from "react";
+import { emptyMeme, MemeSVGViewer, type ImageInterface, type MemeInterface } from "orsys-tjs-meme";
+import { useEffect, useState } from "react";
+import { REST_ADR, REST_RESSOURCES } from "./constantes/config";
 
 function App() {
   const [current, setCurrent] = useState<MemeInterface>(emptyMeme)
+  const [images, setimages] = useState<Array<ImageInterface>>([]);
+  useEffect(() => {
+    fetch(`${REST_ADR}${REST_RESSOURCES.images}`).then(r=>r.json()).then(arr=>setimages(arr))
+  }, [])
+  
   return <div className="App">
+    {JSON.stringify(images)}
     <FlexV3rdGrow>
 
       <Header/>
       <Navbar/>
       <FlexH1stGrow>
-        <MemeSVGViewer image={undefined} meme={current} basePath=""/>
+        <MemeSVGViewer image={images.find((img)=>img.id===current.imageId)} meme={current} basePath=""/>
         <MemeForm meme={current} onMemeChange={(newMeme:MemeInterface)=>{
           setCurrent(newMeme)
         }}/>
