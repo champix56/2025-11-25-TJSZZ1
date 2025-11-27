fbb8ba5a11c411a3f1c3bad85b2261d79932c682
champix56 <desorbaix@free.fr>
2025-11-27 15:39:31 +0100

    fin connexion

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,39 +1,12 @@
 import "./App.css";
-import './store/store.ts'
 import FlexV3rdGrow from "./components/layout/FlexV3rdGrow/FlexV3rdGrow";
 import Navbar from "./components/ui/Navbar/Navbar";
 import Header from "./components/ui/Header/Header";
 import FlexH1stGrow from "./components/layout/FlexH1stGrow/FlexH1stGrow";
-import MemeForm from "./components/MemeForm/MemeForm";
+import MemeForm from "./components/MemeForm/MemeForm.connected";
 import Footer from "./components/ui/Footer/Footer";
 import MemeSVGViewer from './components/ui/MemeSVGViewer/MemeSVGViewer.connected'
-import {
-  emptyMeme,
-  type ImageInterface,
-  type MemeInterface,
-} from "orsys-tjs-meme";
-import { useEffect, useState } from "react";
-import { REST_ADR, REST_RESSOURCES } from "./constantes/config";
-
 function App() {
-  const [current, setCurrent] = useState<MemeInterface>(emptyMeme);
-  const [images, setimages] = useState<Array<ImageInterface>>([]);
-  useEffect(() => {
-    const prImages = fetch(`${REST_ADR}${REST_RESSOURCES.images}`).then((r) =>
-      r.json()
-    );
-    const prTimeOut = new Promise((res) => {
-      setTimeout(() => res(null), 500);
-    });
-    const prRace = Promise.race([prImages, prTimeOut]);
-    prRace.then((arr) => {
-      if (arr === null) {
-        return console.log("timeOut");
-      }
-      setimages(arr);
-    });
-  }, []);
-
   return (
     <div className="App">
       <FlexV3rdGrow>
@@ -41,13 +14,7 @@ function App() {
         <Navbar />
         <FlexH1stGrow>
           <MemeSVGViewer/>
-          <MemeForm
-          images={images}
-            meme={current}
-            onMemeChange={(newMeme: MemeInterface) => {
-              setCurrent(newMeme);
-            }}
-          />
+          <MemeForm/>
         </FlexH1stGrow>
         <Footer />
       </FlexV3rdGrow>
